171c7695566037515f5cac03aa0a7b89074e1d76
Marks Duarte <marksduarte@gmail.com>
2020-06-15 22:59:34 -0300

    Criando uma página de profile

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,12 +4,12 @@ import { CredenciaisDTO } from "../models/credenciais.dto";
 import { API_CONFIG } from "../config/api.config";
 import { LocalUser } from "../models/local_user";
 import { StorageService } from "./storage.service";
-import { JwtHelperService } from "@auth0/angular-jwt";
+import { JwtHelper } from "angular2-jwt";
 
 @Injectable()
 export class AuthService {
 
-  jwtHelper: JwtHelperService = new JwtHelperService();
+  jwtHelper: JwtHelper = new JwtHelper();
 
   constructor(public http: HttpClient, public storage: StorageService) {
 
