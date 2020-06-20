9fe5d5ead2b24c7ae58b1199f6e49407f1c63108
Marks Duarte <marksduarte@gmail.com>
2020-06-19 23:17:22 -0300

    Tratamento padrão para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,12 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS
 import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
 import { StorageService } from "../services/storage.service";
+import { AlertController } from "ionic-angular";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(public storage : StorageService){}
+  constructor(public storage : StorageService, public alertCtrl : AlertController){}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).catch((error, caught) => {
@@ -21,8 +22,14 @@ export class ErrorInterceptor implements HttpInterceptor {
       console.log(errorObj);
       // Para tratar erros específicos
       switch(errorObj.status) {
+        case 401:
+          this.handle401();
+          break;
         case 403:
           this.handle403();
+          break;
+        default:
+          this.handleDefaultError(errorObj);
           break;
       }
 
@@ -30,9 +37,33 @@ export class ErrorInterceptor implements HttpInterceptor {
     }) as any;
   }
 
+  handle401() {
+    let alert = this.alertCtrl.create({
+      title: 'Falha de autenticação',
+      message: 'Email ou senha incorretos',
+      enableBackdropDismiss: false,
+      buttons: [
+        {text: 'Ok'}
+      ]
+    });
+    alert.present();
+  }
+
   handle403() {
     // caso não autenticado, remove o possível localUser armazenado.
     this.storage.setLocalUser(null);
+  }
+
+  handleDefaultError(errorObj) {
+    let alert = this.alertCtrl.create({
+      title: errorObj.status + ': ' + errorObj.error,
+      message: errorObj.message,
+      enableBackdropDismiss: false,
+      buttons: [
+        {text: 'Ok'}
+      ]
+    });
+    alert.present();
   }
 }
 
--- src/services/domain/categoria.service.ts
+++ src/services/domain/categoria.service.ts
@@ -7,11 +7,11 @@ import { CategoriaDTO } from '../../models/categoria.dto';
 @Injectable()
 export class CategoriaService {
 
-    constructor(public http: HttpClient){
+  constructor(public http: HttpClient) {
 
-    }
+  }
 
-    findAll() : Observable<CategoriaDTO[]> {
-        return this.http.get<CategoriaDTO[]>(`${API_CONFIG.baseUrl}/categorias`);
-    }
+  findAll(): Observable<CategoriaDTO[]> {
+    return this.http.get<CategoriaDTO[]>(`${API_CONFIG.baseUrl}/categorias`);
+  }
 }
