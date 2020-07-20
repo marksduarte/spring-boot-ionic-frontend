fb85d37839e6e48053ce514ddafa26d33d143e00
Marks Duarte <marksduarte@gmail.com>
2020-07-20 00:03:15 -0300

    Criando página de carrinho de compras

--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
+import { CartService } from '../../services/domain/cart.service';
 
 @IonicPage()
 @Component({
@@ -13,24 +14,33 @@ export class ProdutoDetailPage {
 
   item: ProdutoDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public produtoService: ProdutoService) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public produtoService: ProdutoService,
+    public cartService: CartService) {
   }
 
   ionViewDidLoad() {
     let produto_id = this.navParams.get('produto_id');
     this.produtoService.findbyId(produto_id)
-    .subscribe(response => {
-      this.item = response;
-      this.getImageUrlIfExists();
-    },
-    error => {});
+      .subscribe(response => {
+        this.item = response;
+        this.getImageUrlIfExists();
+      },
+        error => { });
   }
 
   getImageUrlIfExists() {
     this.produtoService.getImageFromBucket(this.item.id).subscribe(response => {
       this.item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.item.id}.jpg`;
     },
-    error => {});
+      error => { });
+  }
+
+  addToCart(produto: ProdutoDTO) {
+    this.cartService.addProduto(produto);
+    this.navCtrl.setRoot('CartPage');
   }
 
 }
